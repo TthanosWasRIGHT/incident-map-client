9d5653841afb749a71b81b1823082c04babe35ee
Cornel Ngome <Cornel.Ngome@securex.local>
2025-07-19 11:01:29 +0300

    Add vite.config.js

---
+++ src/vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: './', // required for Netlify to serve the app correctly
+})
